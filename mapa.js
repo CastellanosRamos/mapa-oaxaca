2fc5c7fdfbb0248fdbc051850ee9a58034a9060d
Tu Nombre <tu-email@ejemplo.com>
2026-04-16 16:03:13 -0600

    municipios

--- mapa.js
+++ mapa.js
@@ -1,16 +1,15 @@
-let map = L.map('map').setView([17.060555555556, -96.725277777778],7)
+let map = L.map('map').setView([17.060555555556, -96.725277777778], 8)
 
 //Agregar tilelAyer mapa base desde openstreetmap
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
-  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; OpenStreetMap'
 }).addTo(map);
 
-document.getElementById('select-location').addEventListener('change',function(e){
+document.getElementById('select-location').addEventListener('change', function(e) {
   let coords = e.target.value.split(",");
-  map.flyTo(coords,13);
+  map.flyTo(coords, 13);
 })
 
-//En esta sección se xploran los layer groups, que son grupos de capas que se pueden manejar como una sola capa. Esto es útil para organizar y controlar la visibilidad de múltiples capas en un mapa. En este ejemplo, se crean varios marcadores individuales y luego se agrupan en un layer group llamado "cities". Finalmente, el layer group se agrega al mapa para mostrar todos los marcadores juntos.
 // Crear marcadores individuales
 let tlacolula = L.marker([16.957644318128274, -96.48049111696808]).bindPopup(`
   <strong>Tlacolula de Matamoros</strong><br>
@@ -19,14 +18,31 @@ let tlacolula = L.marker([16.957644318128274, -96.48049111696808]).bindPopup(`
   <strong>Telefono:</strong>  951-14-4-32-49
 `);
 
-
-
 let ocotlan = L.marker([16.792222, -96.675]).bindPopup("Ocotlan de Morelos");
-let zimatlan = L.marker([16.77, -96.95]).bindPopup("Aurora");
+let zimatlan = L.marker([16.77, -96.95]).bindPopup("Zimatlan de Alvarez");
 let miahuatlan = L.marker([16.3327, -96.59558]).bindPopup("Miahuatlan");
+let tehuantepec = L.marker([16.31841,-95.24783]).bindPopup("Tehuantepec");
+let matias = L.marker([16.8754,-95.04106]).bindPopup("Matias Romero");
+let teotitlan = L.marker([18.1306,-97.06839]).bindPopup("Teotitlan de Flores Magon");
+let nochixtlan = L.marker([18.1306,-97.06839]).bindPopup("Nochixtlan");
+let tlaxiaco = L.marker([17.2700, -97.67790]).bindPopup("Tlaxiaco");
+let huajuapan = L.marker([17.8042, -97.78069]).bindPopup("Huajuapan de León");
+let pinotepa = L.marker([16.3381, -98.04850]).bindPopup("Pinotepa Nacional");
+let puerto = L.marker([15.8658, -97.06923]).bindPopup("Puerto Escondido");
+let ixtlan = L.marker([17.3341, -96.4867]).bindPopup("Ixtlan de Juarez");
+let tuxtepec = L.marker([18.0936, -96.13147]).bindPopup("Tuxtepec");
+let ayutla = L.marker([18.0936, -96.13147]).bindPopup("Ayutla Mixe");
 
-// 2. Crear un layer group con los marcadores
-var cities = L.layerGroup([tlacolula, ocotlan, zimatlan, miahuatlan]);
 
-// 3. Agregar el layer group al mapa
-cities.addTo(map);
+// Crear un layer group con los marcadores
+var coordinaciones = L.layerGroup([tlacolula, ocotlan, zimatlan, miahuatlan, tehuantepec, matias, teotitlan, nochixtlan, tlaxiaco, huajuapan, pinotepa, puerto, ixtlan, tuxtepec, ayutla]);
+
+// Agregar el layer group al mapa
+coordinaciones.addTo(map);
+
+// === AGREGAR CONTROL DE CAPAS ===
+let overlayMaps = {
+  "Coordinaciones": coordinaciones
+};
+
+L.control.layers(null, overlayMaps).addTo(map);
